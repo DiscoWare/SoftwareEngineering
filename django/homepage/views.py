e0d6fa3ef665994d2241d0fa28edc9d48715b784
pbaut002 <peter.g.bautista@gmail.com>
2019-06-10 13:39:35 -0700

    Numbered pages

--- django/homepage/views.py
+++ django/homepage/views.py
@@ -12,7 +12,7 @@ def homepage(request):
 	paginator = Paginator(item_list, 1)
 	page = request.GET.get('page')
 	Items = paginator.get_page(page)
-
+	
 	return render(request=request, 
 				  template_name="homepage/home.html", # Where to find the html file
 				  context={"tutorials": Tutorial.objects.all,"Items":Items}) # We can pass Tutorial objects via the name "tutorials"
